386bbd18f0a5f8fc71ba5a288b1bf4bb2b5646b6
MustafaTaherZaky <mustafataherzaky@outlook.com>
2019-10-06 15:02:52 +0200

    project added

--- Arduino/008_2_IR_Remote_RGB_modes/008_2_IR_Remote_RGB_modes.ino
+++ Arduino/008_2_IR_Remote_RGB_modes/008_2_IR_Remote_RGB_modes.ino
@@ -9,40 +9,27 @@ decode_results result;
 #define g 5
 #define b 6
 
-int counter = 0 ; 
-unsigned int colors[8] = {C_0 , C_1,C_2,C_3,C_4,C_5,C_6,C_7};
-unsigned int last_result = 0 ;
-unsigned int last_result_2 = 0 ; 
-
+int color_num = 0 ; 
 bool mode = false ; 
+String colors[8] = {"None" , "Red", "Green" , "Blue" ,"Yellow" , "Cyan" , "Purple","White" }; 
 
 void setup (){
-
   Serial.begin(9600);
   setup_RGB();
   setup_receiver();
-  
   }
 
 void loop  (){
-  if (irrecv.decode(&result)) { 
-      last_result = result.value ;
-      irrecv.resume(); // Receive the next value     
-     }
-  if (mode == true){
-  set_color(last_result);
-  delay(50);
-  set_color(C_0);
-  delay(50);}
-  else
-  {set_color(last_result);}
+  get_remote_and_set_color_num();
+  RGB_Control();
 }
+
+
+ 
 void setup_RGB(){
-     
   pinMode(r, OUTPUT);     
   pinMode(g, OUTPUT);     
   pinMode(b, OUTPUT);     
-
 }
 
 void setup_receiver(){  
@@ -51,97 +38,121 @@ void setup_receiver(){
   Serial.println("Enabled IRin");
 }
 
-void set_color(unsigned int value){
+void get_remote_and_set_color_num(){
+  if (irrecv.decode(&result)) { 
+      set_color_num(result.value);
+      irrecv.resume(); // Receive the next value     
+     }
+     }
+     
+void RGB_Control(){
+  if (mode == true){
+  set_color(colors[color_num]);
+  delay(50);
+  set_color(colors[0]);
+  delay(50);}
+  else
+  {set_color(colors[color_num]);}
+  }
+
+void set_color_num(unsigned int value){
  switch(value) {
   case StillPressed : 
     //Serial.println("StillPressed");  
     break;
   case C_CHm : 
-    Serial.println("NextColor"); 
-    counter= (counter-1+8)%8;
-    set_color(colors[counter]);
-    last_result = colors[counter] ; 
+    Serial.println("LastColor"); 
+    color_num = (color_num -1+8)%8;
+    Serial.println(colors[color_num]);
     break;
   case C_CHp : 
-    Serial.println("LastColor");  
-    counter= (counter+1)%8;
-    set_color(colors[counter]);
-    last_result = colors[counter] ;
+    Serial.println("NextColor");  
+    color_num = (color_num+1)%8;
+    Serial.println(colors[color_num]);
     break;
-        
-  case C_0 : 
-    Serial.println("None");
-    digitalWrite(r,LOW);
-    digitalWrite(g,LOW);
-    digitalWrite(b,LOW);
-    if (mode == false)  
-    {last_result = C_0 ;
-    counter = 0 ;}
-    break;          
+    
   case C_EQ : 
     Serial.println("Mode Changed");
     mode = !mode ; 
-    last_result = colors[counter] ;
     break; 
+  
+  case C_0 : 
+    color_num = 0 ;
+    Serial.println(colors[0]);
+    break;          
              
-  case C_1 : 
-    Serial.println("RED");  
-    digitalWrite(r,HIGH);
-    digitalWrite(g,LOW);
-    digitalWrite(b,LOW);
-    last_result = C_1 ;
-    counter = 1;
+  case C_1 :
+    color_num = 1;
+    Serial.println(colors[1]);
     break;          
   case C_2 : 
-    Serial.println("Green");  
-    digitalWrite(r,LOW);
-    digitalWrite(g,HIGH);
-    digitalWrite(b,LOW);
-    last_result = C_2 ;
-    counter = 2 ;
+    color_num = 2 ;
+    Serial.println(colors[2]);
     break;          
-  case C_3 : 
-    Serial.println("Blue"); 
-    digitalWrite(r,LOW);
-    digitalWrite(g,LOW);
-    digitalWrite(b,HIGH);
-    last_result = C_3 ;
-    counter = 3 ;  
+  case C_3 :
+    color_num = 3 ;  
+    Serial.println(colors[3]);
     break;          
-  case C_4 : 
-    Serial.println("Yellow");  
-    digitalWrite(r,HIGH);
-    digitalWrite(g,HIGH);
-    digitalWrite(b,LOW);
-    last_result = C_4 ;
-    counter = 4 ;
+  case C_4 :   
+    color_num = 4 ;
+    Serial.println(colors[4]);
     break;          
-  case C_5 : 
-    Serial.println("Cyan");
-    digitalWrite(r,LOW);
-    digitalWrite(g,HIGH);
-    digitalWrite(b,HIGH);
-    last_result = C_5 ;
-    counter = 5 ;  
+  case C_5 :
+    color_num = 5 ;  
+    Serial.println(colors[5]);
     break;          
   case C_6 : 
-    Serial.println("Purle");
-    digitalWrite(r,HIGH);
-    digitalWrite(g,LOW);
-    digitalWrite(b,HIGH);
-    last_result = C_6 ; 
-    counter = 6 ; 
+    color_num = 6 ; 
+    Serial.println(colors[6]);
     break;          
   case C_7 : 
-    Serial.println("White"); 
-    digitalWrite(r,HIGH);
-    digitalWrite(g,HIGH);
-    digitalWrite(b,HIGH);
-    last_result = C_7 ;
-    counter = 7; 
+    color_num = 7;
+    Serial.println(colors[7]); 
     break;          
  // default:
     //Serial.println("Unknown");
   } 
 }
-  
+
+void set_color(String color)
+{
+  if (color == "Red")
+      {digitalWrite(r,HIGH);
+      digitalWrite(g,LOW);
+      digitalWrite(b,LOW);} 
+     
+  else if(color == "Green")
+      {digitalWrite(r,LOW);
+      digitalWrite(g,HIGH);
+      digitalWrite(b,LOW); 
+      }
+  else if(color == "Blue")
+      {digitalWrite(r,LOW);
+      digitalWrite(g,LOW);
+      digitalWrite(b,HIGH); 
+      }
+  else if(color == "Yellow")
+      {digitalWrite(r,HIGH);
+      digitalWrite(g,HIGH);
+      digitalWrite(b,LOW); 
+      }
+  else if(color == "Cyan")
+      {digitalWrite(r,LOW);
+      digitalWrite(g,HIGH);
+      digitalWrite(b,HIGH); 
+      }
+  else if(color == "Purple")
+      {digitalWrite(r,HIGH);
+      digitalWrite(g,LOW);
+      digitalWrite(b,HIGH); 
+      }
+  else if(color == "White")
+      {digitalWrite(r,HIGH);
+      digitalWrite(g,HIGH);
+      digitalWrite(b,HIGH); 
+      }
+  else {
+      digitalWrite(r,LOW);
+      digitalWrite(g,LOW);
+      digitalWrite(b,LOW);}
+  }
